a6eb23d4354d37cffaa7b7f513d5d5829dfb6aac
FlintZheng <zyg80878200@gmail.com>
2020-03-20 22:33:53 +0800

    testCase

--- src/test/java/com/irobotzz/cache/app/AppTest.java
+++ src/test/java/com/irobotzz/cache/app/AppTest.java
@@ -50,4 +50,29 @@ public class AppTest
 			// Clear all cache
 			util.clear();
 	    }
+	    /**
+	     * Create the test case
+	     *
+	     * @param testName name of the test case
+	     */
+	    public AppTest( String testName )
+	    {
+	        super( testName );
+	    }
+
+	    /**
+	     * @return the suite of tests being tested
+	     */
+	    public static Test suite()
+	    {
+	        return new TestSuite( AppTest.class );
+	    }
+
+	    /**
+	     * Rigourous Test :-)
+	     */
+	    public void testApp()
+	    {
+	        assertTrue( true );
+	    }
 }
